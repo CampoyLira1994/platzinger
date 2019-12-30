c1e15731f092e79c736e417236d1a7973219e87f
jcampoy@centraldealarmas.com.mx <jcampoy@centraldealarmas.com.mx>
2019-12-30 16:39:28 -0600

    Primer Commit

---
+++ src/app/hola/hola.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hola',
+  templateUrl: './hola.component.html',
+  styleUrls: ['./hola.component.css']
+})
+export class HolaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
